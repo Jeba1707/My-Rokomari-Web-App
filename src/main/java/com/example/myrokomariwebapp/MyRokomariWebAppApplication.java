284c907495a6705406e948ccd5e9fdae94963b56
Jeba1707 <jebapc2023@gmail.com>
2024-05-10 11:05:27 +0600

    Initial Commit

---
+++ src/main/java/com/example/myrokomariwebapp/MyRokomariWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrokomariwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRokomariWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRokomariWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myrokomariwebapp/MyRokomariWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrokomariwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRokomariWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
